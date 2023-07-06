d5900659e73beb236cacf9c06636fe09c28a648a
BilalLiaquat7 <bilal.liaquat7@gmail.com>
2023-07-06 20:43:41 +0500

    linters removed

--- menu.js
+++ menu.js
@@ -1,12 +1,11 @@
-const hamburger = document.getElementById("hamburger");
-const menuIcon = document.getElementById("menu");
-const btnCross = document.getElementById("cross");
+const hamburger = document.getElementById('hamburger');
+const menuIcon = document.getElementById('menu');
+const btnCross = document.getElementById('cross');
 
-hamburger.addEventListener("click", () => {
-  console.log("I'm clicked");
-  menuIcon.style.display = menuIcon.style.display === "flex" ? "none" : "flex";
+hamburger.addEventListener('click', () => {
+  menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
 });
 
-btnCross.addEventListener("click", () => {
-  menuIcon.style.display = menuIcon.style.display === "flex" ? "none" : "flex";
+btnCross.addEventListener('click', () => {
+  menuIcon.style.display = menuIcon.style.display === 'flex' ? 'none' : 'flex';
 });
